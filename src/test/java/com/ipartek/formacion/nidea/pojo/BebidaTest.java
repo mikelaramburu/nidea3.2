299ba36156f2a851949f549901d8d4904347920a
anderuraga <ander.uraga@gmail.com>
2018-04-11 11:06:36 +0200

    test con nuevos metodos para retulizar codigo

--- src/test/java/com/ipartek/formacion/nidea/pojo/BebidaTest.java
+++ src/test/java/com/ipartek/formacion/nidea/pojo/BebidaTest.java
@@ -1,8 +1,10 @@
 package com.ipartek.formacion.nidea.pojo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -10,20 +12,46 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BebidaTest {
+
+	static ValidatorFactory factory;
+	static Validator validator;
+	static Bebida bebida;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		// Crear Factoria y Validador
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		factory = null;
+		validator = null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		bebida = new Bebida();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		bebida = null;
+	}
 
 	@Test
 	public void validar() {
 
 		try {
-
-			// Crear Factoria y Validador
-			ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-			Validator validator = factory.getValidator();
-
-			Bebida bebida = new Bebida();
 
 			Set<ConstraintViolation<Bebida>> violations = validator.validate(bebida);
 
@@ -32,6 +60,30 @@ public class BebidaTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void validarNombre() {
+
+		bebida.setPrecio(0.2f);
+		bebida.setNombre("ab");
+
+		Set<ConstraintViolation<Bebida>> violations = validator.validate(bebida);
+
+		// si ponemos precio correcto, solo falla el nombre que es == "ab"
+
+		violations = validator.validate(bebida);
+		assertTrue(violations.size() == 1);
+
+		Iterator<ConstraintViolation<Bebida>> it = violations.iterator();
+		ConstraintViolation<Bebida> violation = null;
+		while (it.hasNext()) {
+			violation = it.next();
+			assertEquals("nombre", violation.getPropertyPath().toString());
+			assertEquals("ab", violation.getInvalidValue());
+			// violation.getMessage();
 		}
 
 	}
